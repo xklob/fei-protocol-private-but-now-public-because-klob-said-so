fc5ee511b0a8017cf0c73a13789e165f7bedabce
thomas-waite <thomas.waite4@gmail.com>
2022-08-01 13:58:12 +0100

    feat: add multi party timelocked delegator:'

---
+++ contracts/timelocks/MultiPartyTimelockedDelegator.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import {TimelockedDelegator} from "./TimelockedDelegator.sol";
+
+/// @title Multi party timelocked delegator
+/// @notice Timelocked delegator that allows one party to claim vesting TRIBE and another
+///         to manage delegations
+contract MultiPartyTimelockedDelegator is TimelockedDelegator {
+    event SetDelegationManager(address indexed _oldDelegationManager, address indexed _newDelegationManager);
+
+    /// @notice Manager giving out delegations
+    address public delegationManager;
+
+    constructor(
+        address _tribe,
+        address _beneficiary,
+        uint256 _duration,
+        address _delegationManager
+    ) TimelockedDelegator(_tribe, _beneficiary, _duration) {
+        delegationManager = _delegationManager;
+    }
+
+    modifier onlyDelegationManagerOrBeneficiary() {
+        require(
+            msg.sender == delegationManager || msg.sender == beneficiary,
+            "TokenTimelock: Caller not delegation manager or beneficiary"
+        );
+        _;
+    }
+
+    /// @notice Set a new delegation manager, permissioned to the beneficiary
+    function setDelegationManager(address _newDelegationManager) external onlyBeneficiary {
+        address oldDelegationManager = delegationManager;
+
+        // Set new delegation manager in storage
+        delegationManager = _newDelegationManager;
+        emit SetDelegationManager(oldDelegationManager, _newDelegationManager);
+    }
+
+    /// @notice Pass through delegate method, permissioned to the delegate manager or beneficiary
+    function delegate(address delegatee, uint256 amount) public override onlyDelegationManagerOrBeneficiary {
+        _delegate(delegatee, amount);
+    }
+
+    /// @notice Pass through undelegate method, permissioned to the delegation manager or beneficiary
+    function undelegate(address delegatee) public override onlyDelegationManagerOrBeneficiary returns (uint256) {
+        return _undelegate(delegatee);
+    }
+}
