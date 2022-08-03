8d52e8cbd64c65f4acac6f82a8d39748b7104eae
thomas-waite <thomas.waite4@gmail.com>
2022-08-03 16:36:53 +0100

    refactor: break out rari infra proposal, update block num

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -2,29 +2,9 @@ import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/typ
 
 import fip_x from '@proposals/description/fip_x';
 
-import tip_119 from '@proposals/description/tip_119';
 import ido_liquidity_removal from '@proposals/description/ido_liquidity_removal';
-import rari_infra_clawback from '@proposals/description/rari_infra_clawback';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
-  tip_119: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: tip_119, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-    proposalId: '',
-    affectedContractSignoff: [],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.TC
-  },
-  rari_infra_clawback: {
-    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: rari_infra_clawback, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-    proposalId: '',
-    affectedContractSignoff: [],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.TC
-  },
   ido_liquidity_removal: {
     deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
