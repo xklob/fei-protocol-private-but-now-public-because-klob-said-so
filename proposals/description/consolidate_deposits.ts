1544b01cd0dbc97e712780ed1e0534466745aebd
thomas-waite <thomas.waite4@gmail.com>
2022-08-03 18:55:41 +0100

    feat: scaffolding of proposal

---
+++ proposals/description/consolidate_deposits.ts
@@ -0,0 +1,19 @@
+import { TemplatedProposalDescription } from '@custom-types/types';
+
+const consolidate_deposits: TemplatedProposalDescription = {
+  title: 'Phase 3a: Consolidate deposits',
+  commands: [
+    {
+      target: '',
+      values: '',
+      method: '',
+      arguments: (addresses) => [],
+      description: ''
+    }
+  ],
+  description: `
+  Phase 3a: Consolidate deposits
+  `
+};
+
+export default consolidate_deposits;
