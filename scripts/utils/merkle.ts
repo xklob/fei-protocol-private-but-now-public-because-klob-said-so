af4328241b39b9d704649c1564af3831daad841f
Caleb <ditchfieldcaleb@gmail.com>
2022-08-10 23:23:14 -0700

    working on merkle roots

--- scripts/utils/merkle.ts
+++ scripts/utils/merkle.ts
@@ -1,12 +1,23 @@
-import { keccak256, solidityKeccak256 } from 'ethers/lib/utils';
-import MerkleTree from 'merkletreejs';
-import merkleWallets from '@proposals/data/ragequit_data.json';
-const hashFn = (data: string) => keccak256(data).slice(2);
-export const createTree = (): MerkleTree => {
-  const elements = Object.entries(merkleWallets).map(([account, balance]) =>
-    solidityKeccak256(['address', 'uint256'], [account, balance])
-  );
-  const tree = new MerkleTree(elements, hashFn, { sort: true });
+import { defaultAbiCoder, keccak256 } from 'ethers/lib/utils';
+import { MerkleTree } from 'merkletreejs';
 
-  return tree;
-};
+const leaf1 = ['0x11e52c75998fe2E7928B191bfc5B25937Ca16741', '1000000000000000000'];
+const leaf2 = ['0x96fa6ACfc5F683Db191234c74D315e5D732b07c0', '1000000000000000000'];
+
+const encodedleaf1 = defaultAbiCoder.encode(['address', 'uint256'], leaf1);
+const encodedleaf2 = defaultAbiCoder.encode(['address', 'uint256'], leaf2);
+
+const leaves = [encodedleaf1, encodedleaf2].map((x) => keccak256(x));
+
+const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+
+const root = tree.getRoot();
+
+const proof1 = tree.getHexProof(encodedleaf1);
+const proof2 = tree.getHexProof(encodedleaf2);
+
+const verified1 = tree.verify(proof1, encodedleaf1, root);
+const verified2 = tree.verify(proof2, encodedleaf2, root);
+
+console.log(`Leaf 1 Verified: ${verified1}`);
+console.log(`Leaf 2 Verified: ${verified2}`);
