700289312369bad8c3327ac1697bc429a5a846fd
Erwan Beauvois <erwan@allmyt.eu>
2022-08-12 15:25:46 +0200

    Add tip_121.ts empty files

---
+++ proposals/description/tip_121.ts
@@ -0,0 +1,16 @@
+import { ethers } from 'ethers';
+import { TemplatedProposalDescription } from '@custom-types/types';
+
+const tip_121: TemplatedProposalDescription = {
+  title: 'TIP-121: PCV Consolidation (phase 1.c)',
+  commands: [
+    // TODO
+  ],
+  description: `
+  TIP-121: PCV Consolidation (phase 1.c)
+
+  TODO
+  `
+};
+
+export default tip_121;
