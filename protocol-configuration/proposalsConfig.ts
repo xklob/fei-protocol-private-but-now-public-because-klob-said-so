68f4cb839b9d27c66ab2631fc033afa05afec7e9
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2022-08-21 18:01:08 -0700

    better naming

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -1,12 +1,12 @@
 import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/types';
 
-import part2 from '@proposals/description/part2';
+import phase2 from '@proposals/description/part2';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
   tip_119: {
     deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: part2, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposal: phase2, // full proposal file, imported from '@proposals/description/fip_xx.ts'
     proposalId: '',
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
