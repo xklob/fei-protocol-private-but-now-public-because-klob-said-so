2cc15dc6a74c43f2b48042e4990fb08d9aa9a7d6
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2022-08-26 14:54:58 -0700

    deploy contract

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -4,7 +4,7 @@ import phase2 from '@proposals/description/phase2';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
   phase2: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
     proposal: phase2, // full proposal file, imported from '@proposals/description/fip_xx.ts'
     proposalId: '',
