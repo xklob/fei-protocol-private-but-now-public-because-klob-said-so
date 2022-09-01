d1570c68f9361e20e3783794d6d21e155e231dd6
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2022-09-01 15:48:55 -0700

    setTokenBalance issue - frozen commit'

--- contracts/test/utils/ExternalTest.sol
+++ contracts/test/utils/ExternalTest.sol
@@ -3,13 +3,21 @@ pragma solidity ^0.8.4;
 
 import "../libs/forge-standard/src/Test.sol";
 
-// Wrappers around Cheatcodes to avoid footguns
 contract ExternalTest is Test {
     function getTokens(
         address token,
         address to,
         uint256 give
     ) external {
-        deal(token, to, give);
+        require(isContract(address(vm)), "VM contract has zero size!");
+        deal(token, to, give, true);
+    }
+
+    function isContract(address _addr) internal view returns (bool) {
+        uint32 size;
+        assembly {
+            size := extcodesize(_addr)
+        }
+        return (size > 0);
     }
 }
