9521280e400aadb38e21ef1c35a7a9cc9c901ac1
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2022-09-01 14:08:01 -0700

    fix proposalsconfig

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -5,7 +5,7 @@ import tip_121b from '@proposals/description/tip_121b';
 import tip_121c from '@proposals/description/tip_121c';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
-  phase2: {
+  tip_121b: {
     deploy: true,
     totalValue: 0,
     proposal: tip_121b,
@@ -14,7 +14,7 @@ export const ProposalsConfig: TemplatedProposalsConfigMap = {
     deprecatedContractSignoff: [],
     category: ProposalCategory.DAO
   },
-  tip_121b: {
+  tip_121c: {
     deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
     proposal: tip_121c, // full proposal file, imported from '@proposals/description/fip_xx.ts'
