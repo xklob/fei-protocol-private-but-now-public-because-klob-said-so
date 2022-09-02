1aa59f950953d6ba2bdb755df0c86881e8865896
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2022-09-01 17:38:34 -0700

    normalize some scripts to bash standards; in progress

--- scripts/shutdown/deployMerkleRedeemer.ts
+++ scripts/shutdown/deployMerkleRedeemer.ts
@@ -2,21 +2,53 @@ import { RariMerkleRedeemer__factory } from '../../types/contracts';
 import { MainnetContractsConfig } from '../../protocol-configuration/mainnetAddresses';
 import { ethers } from 'ethers';
 import { cTokens } from './data/sample/cTokens';
-import { rates } from './data/sample/rates';
-import { roots } from './data/sample/roots';
+import sampleRates from './data/sample/rates.json';
+import sampleRoots from './data/sample/roots.json';
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider('http://nodeinator.kryptoklob.io:8999');
+  if (process.argv[2] === 'help') {
+    console.log(`
+      Usage: 
+        npx ts-node scripts/shutdown/deployMerkleRedeemer [ratesJSONFileName] [rootsJSONFileName] [forkMode]
+      
+      Args:
+        ratesJSONFileName = relative or absolute file locator string (default: "./data/sample/rates.json")
+        rootsJSONFileName = relative or absolute file locator string (default: "./data/sample/roots.json")
+        forkMode = true | false (default: true)
+
+      Examples: 
+        npx ts-node scripts/shutdown/deployMerkleRedeemer
+        npx ts-node scripts/shutdown/deployMerkleRedeemer ./data/actual/rates.json
+        npx ts-node scripts/shutdown/deployMerkleRedeemer ./data/actual/rates.json ./data/actual/roots.json
+        npx ts-node scripts/shutdown/deployMerkleRedeemer ./data/actual/rates.json ./data/actual/roots.json true
+    `);
+    return;
+  }
+
+  // todo: sanity check rates & roots
+
+  let provider: ethers.providers.JsonRpcProvider;
+
+  if (process.argv[4] === 'false') {
+    provider = new ethers.providers.JsonRpcBatchProvider('http://nodeinator.kryptoklob.io:8545');
+  } else {
+    provider = new ethers.providers.JsonRpcProvider('http://nodeinator.kryptoklob.io:8999');
+  }
+
   await provider.ready;
   const wallet = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider);
   const rariMerkleRedeemerFactory = new RariMerkleRedeemer__factory(wallet);
+
   const rariMerkleRedeemer = await rariMerkleRedeemerFactory.deploy(
     MainnetContractsConfig.fei.address,
     cTokens,
-    rates,
-    roots
+    sampleRates,
+    sampleRoots
   );
+
   console.log(`Rari Merkle Redeemer deployed to ${rariMerkleRedeemer.address}`);
+
+  // todo: verify if *not* fork
 }
 
 main();
