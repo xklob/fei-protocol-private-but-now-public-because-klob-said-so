7dfe1be4a05f579e70fe6a0d8ce9d5949a78827a
Jamie Rumbelow <jamie@jamierumbelow.net>
2022-09-05 15:56:08 +0100

    DEV-3039: various untested scenario scripts

---
+++ scripts/shutdown/test-scenarios/002-one-ctoken.sh
@@ -0,0 +1,4 @@
+npx ts-node scripts/shutdown/resetFork
+npx ts-node scripts/shutdown/setTokenBalance 0xd8553552f8868c1ef160eedf031cf0bcf9686945 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 1 true
+npx ts-node scripts/shutdown/createMerkleTree scripts/shutdown/data/sample/snapshot.json scripts/shutdown/data/test/roots.json true scripts/shutdown/data/sample/testingSnapshot.json
+npx ts-node scripts/shutdown/deployMerkleRedeemer scripts/shutdown/data/sample/rates.json scripts/shutdown/data/test/roots.json
