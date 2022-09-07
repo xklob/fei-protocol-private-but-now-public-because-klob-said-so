962934e79b25549fca4b96b1f7ae2819b2c0efce
Caleb <ditchfieldcaleb@gmail.com>
2022-09-06 18:31:56 -0700

    Add dripper contract

---
+++ contracts/shutdown/fuse/MerkleRedeemerDripper.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity =0.8.10;
+
+import {ERC20Dripper} from "../../pcv/utils/ERC20Dripper.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MerkleRedeemerDripper is ERC20Dripper {
+    constructor(
+        address _core,
+        address _target,
+        uint256 _frequency, // note: actually period (1/frequency)
+        uint256 _amountToDrip,
+        address _token
+    ) ERC20Dripper(_core, _target, _frequency, _amountToDrip, _token) {}
+
+    /// @notice Overrides drip() in the ERC20Dripper contract to add a balance check on the target
+    function drip() public override {
+        require(
+            IERC20(token).balanceOf(target) < amountToDrip,
+            "MerkleRedeemerDripper: dripper target already has enough tokens."
+        );
+        super.drip();
+    }
+}
